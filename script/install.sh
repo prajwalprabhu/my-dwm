fd062c3f868a3669afb152d0eb21b5e0b66de04f
T Prajwal Prabhu <prajwalprabhu.tellar@gmail.com>
2020-09-29 19:20:06 +0530

    "29-sep-2020"

---
+++ script/install.sh
@@ -0,0 +1,31 @@
+sudo mkdir /usr/bin
+sudo cp -i wifi /usr/bin
+sudo chmod 775 /usr/bin/wifi
+sudo cp -i internet /usr/bin
+sudo chmod 775 /usr/bin/internet
+sudo cp -i nettraf /usr/bin
+sudo chmod 775 /usr/bin/nettraf
+sudo cp -i dmenuunicode /usr/bin
+sudo chmod 775 /usr/bin/dmenuunicode
+sudo cp -i dmenumount /usr/bin
+sudo chmod 775 /usr/bin/dmenumount
+sudo cp -i dmenuumount /usr/bin
+sudo chmod 775 /usr/bin/dmenuumount
+sudo cp -i disk /usr/bin
+sudo chmod 775 /usr/bin/disk
+sudo cp -i battery /usr/bin
+sudo chmod 775 /usr/bin/battery
+sudo cp -i volume /usr/bin
+sudo chmod 775 /usr/bin/volume
+sudo cp -i clock /usr/bin
+sudo chmod 775 /usr/bin/clock
+sudo cp -i memory /usr/bin
+sudo chmod 775 /usr/bin/memory
+sudo cp -i nusage /usr/bin
+sudo chmod 775 /usr/bin/nusage
+echo "Done copying the scripts"
+
+
+
+
+
